836f90ce32fb1ebd94968d27af93e1b818a2d2e6
aruymgaart <apruymgaart@gmail.com>
2021-03-26 18:54:50 -0500

    AR model

---
+++ autoregression/AR.py
@@ -0,0 +1,25 @@
+# AP Ruymgaart
+# https://www.statsmodels.org/stable/examples/notebooks/generated/autoregressions.html
+from statsmodels.tsa.ar_model import AutoReg, ar_select_order
+from tensorFiles import *
+import matplotlib.pyplot as plt
+
+data = tnsrFile2numpy('data.npz')
+datT = data[:,0:80] # training data (AR fit)
+
+series = 3
+ARorder = 2
+
+print('=========== cosine AR(2) ===========')
+mod = AutoReg(datT[series], ARorder, old_names=False)
+res = mod.fit()
+print(dir(res))
+print(res.summary())
+
+p = mod.predict(res.params, end=100)
+plt.title('AR prediction (right of red line=predicted, left=training)')
+plt.plot(p, label='AR(2) predicted')
+plt.plot(data[series], label='True')
+plt.axvline(80,c='r')
+plt.legend()
+plt.show()

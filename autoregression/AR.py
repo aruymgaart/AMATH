570f8c30ec08ede3cb55769812cb2f80768faba2
aruymgaart <apruymgaart@gmail.com>
2021-03-26 19:13:15 -0500

    ar

--- autoregression/AR.py
+++ autoregression/AR.py
@@ -4,22 +4,24 @@ from statsmodels.tsa.ar_model import AutoReg, ar_select_order
 from tensorFiles import *
 import matplotlib.pyplot as plt
 
-data = tnsrFile2numpy('data.npz')
-datT = data[:,0:80] # training data (AR fit)
-
 series = 4
-ARorder = 8
+ARorder = 2
+trainEnd = 70
 
-print('=========== cosine AR(2) ===========')
+data = tnsrFile2numpy('data.npz')
+snames = ['$\\cos(x)$', '$e^{-ax}$', '$e^{ax}$', '$a_1x^5 + a_2x^4 + a_3x^3 + a_4x^2 + a_5 x $', '$\\frac{ 1 - e^{-(p+q)t}  }{  1 + (p/q)e^{-(p+q)t}  }$', '$\\sqrt{x}$']
+datT = data[:,0:trainEnd] # training data (AR fit)
+
+print('===================  AR(',ARorder,') ===================')
 mod = AutoReg(datT[series], ARorder, old_names=False)
 res = mod.fit()
-print(dir(res))
 print(res.summary())
 
 p = mod.predict(res.params, end=100)
 plt.title('AR prediction (right of red line=predicted, left=training)')
 plt.plot(p, label='AR(%d) predicted' % (ARorder))
-plt.plot(data[series], label='True')
-plt.axvline(80,c='r')
+plt.plot(data[series], label='True (%s)' % (snames[series]))
+plt.axvline(trainEnd,c='r')
 plt.legend()
+plt.savefig('images/AR_%d_%d_%d.png' % (ARorder, series, trainEnd), dpi=200, bbox_inches='tight')
 plt.show()
